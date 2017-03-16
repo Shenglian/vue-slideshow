26d301a070d9b222efecca5977f3241901ba9050
sheng <sheng@sheng-onead.local>
2017-03-16 18:09:17 +0800

    update

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # vue-slideshow
 slideshow of Vuejs
+
+
+[參考資料：Make a Simple JavaScript Slideshow without jQuery](https://www.sitepoint.com/make-a-simple-javascript-slideshow-without-jquery/)
---
+++ vue-slideshow.vue
@@ -0,0 +1,178 @@
+<template>
+    <div class="slideshow" 
+      @mouseover="pauseSlideshow"
+      @mouseleave="playSlideshow" 
+    >
+        <div id="slides">
+            <a v-for="(img, index) in imgs" 
+              class="slide" 
+              :class="{ 'active': currentSlide(index) }" 
+              :style="{ 'background-image': `url(${img.url})` }" 
+              :href="img.href">Slide {{ index }}</a>
+        </div>
+
+        <div class="paginations">
+          <div v-for="(p, index) in paginations" 
+            class="pagination"
+            :class="{ 'active': currentSlide(index) }"
+            @click="goToSlide(index)"></div>
+        </div>
+        
+        <div class="prev" @click="previousSlideshow"> < </div>
+        <div class="next" @click="nextSlideshow"> > </div>
+          
+    </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      options: {
+        type: Object,
+      },
+      imgs: {
+        type: Array,
+        required: true,
+        default() {
+          return [];
+        },
+      },
+    },
+    data() {
+      return {
+        currentIndex: 0,
+        autoplay: 5000,
+        paginations: [],
+        timer: null,
+      };
+    },
+    mounted() {
+      this.init();
+      this.paginations = this.imgs.length;
+    },
+    methods: {
+      currentSlide(val) {
+        return val === this.currentIndex;
+      },
+      init() {
+        this.$el.style.display = 'block';
+        this.slides = this.$el.querySelectorAll('#slides .slide');
+        this.timer = setInterval(this.nextSlideshow, this.autoplay);
+      },
+      pauseSlideshow() {
+        clearInterval(this.timer);
+      },
+      playSlideshow() {
+        this.timer = setInterval(this.nextSlideshow, this.autoplay);
+      },
+      previousSlideshow() {
+        this.goToSlide(this.currentIndex - 1);
+      },
+      nextSlideshow() {
+        this.goToSlide(this.currentIndex + 1);
+      },
+      goToSlide(n = 0) {
+        // 1 % 5 = 1, 2 % 5 = 2, 3 % 5 = 3, 4 % 5 = 4, and 5 % 5 = 0
+        this.currentIndex = (this.slides.length + n) % this.slides.length;
+      },
+    },
+  };
+</script>
+
+<style lang="scss" scoped>
+
+$slidesZIndex: 2;
+$paginationsZIndex: 3;
+$arrowsZIndex: 4;
+
+// if javascript is available，
+// it will replace none to block;
+.slideshow {
+  display: none;
+  position: relative;
+}
+
+#slides {
+    position: relative;
+    height: 300px;
+    padding: 0px;
+    margin: 0px;
+    list-style-type: none;
+}
+
+.slide {
+    opacity: 0;
+    position: absolute;
+    left: 0;
+    top: 0;
+    padding: 40px;
+    width: 100%;
+    height: 100%;
+
+    background-color: #333;
+    background-size: cover;
+
+    transition: opacity 1s ease-out;
+
+    cursor: pointer;
+
+    z-index: $slidesZIndex;
+    &.active {
+        opacity: 1;
+    }
+}
+
+.slide {
+    color: #fff;
+    font-size: 40px;
+}
+
+.prev,
+.next {
+  position: absolute;
+  top: 50%;
+
+  font-size: 36px;
+  color: #d3d3d3;
+
+  transform: translateY(-50%);
+  z-index: $arrowsZIndex;
+
+  cursor: pointer;
+}
+
+.prev { left: 30px; }
+.next { right: 30px; }
+
+.paginations {
+  position: absolute;
+  left: 0;
+  right: 0;
+  bottom: 20px;
+
+  display: flex;
+  justify-content: center;
+
+  z-index: $paginationsZIndex;
+
+  .pagination {
+    margin: auto 5px;
+    width: 10px;
+    height: 10px;
+    border-radius: 100%;
+    border: 1px solid #333;
+    background-color: #333;
+
+    cursor: pointer;
+    transition: all, .3s;
+    &:hover {
+      border: 1px solid #999999;
+      background-color: #999999;
+    }
+    &.active {
+      background-color: #fff;
+    }
+  }
+}
+
+</style>
