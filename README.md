cfcda83ddc1a7c0f49917d13a1f0ed15fb7925fc
sheng <sheng@sheng-onead.local>
2017-03-21 16:06:56 +0800

    create a project

--- README.md
+++ README.md
@@ -1,5 +1,27 @@
 # vue-slideshow
-slideshow of Vuejs
 
+> slideshow of Vuejs
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
+
+# build for production and view the bundle analyzer report
+npm run build --report
+
+# run unit tests
+npm run unit
+
+# run all tests
+npm test
+```
 
 [參考資料：Make a Simple JavaScript Slideshow without jQuery](https://www.sitepoint.com/make-a-simple-javascript-slideshow-without-jquery/)
---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="app">
+    <Slideshow
+      :imgs="swiperSlides"
+      :options="slideshowOptions">
+    </Slideshow>
+  </div>
+</template>
+
+<script>
+import Slideshow from './vue-slideshow';
+
+export default {
+  name: 'app',
+  components: {
+    Slideshow,
+  },
+  data() {
+    return {
+      swiperSlides: [
+        {
+          url: 'https://source.unsplash.com/category/buildings/1600x900',
+          href: '/',
+        },
+        {
+          url: 'https://source.unsplash.com/category/food/1600x900',
+        },
+        {
+          url: 'https://source.unsplash.com/category/nature/1600x900',
+        },
+        {
+          url: 'https://source.unsplash.com/category/people/1600x900',
+        },
+        {
+          url: 'https://source.unsplash.com/category/technology/1600x900',
+        },
+      ],
+    };
+  },
+};
+</script>
