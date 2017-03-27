ac843e1147ab0362a4a0faa85166fab07da03043
sheng <sheng@sheng-onead.local>
2017-03-27 18:14:40 +0800

    update

---
+++ test/unit/specs/Slideshow.spec.js
@@ -0,0 +1,29 @@
+import Vue from 'vue';
+import SlideShow from '../../../src/vue-slideshow';
+
+describe('SlideShow - Info', () => {
+  it('SlideShow named slideshow', () => {
+    expect(SlideShow.name).equal('slideshow');
+  });
+});
+
+describe('SlideShow - Data', () => {
+
+  const defaultData = SlideShow.data();
+
+  it('init data === function', () => {
+    expect(typeof SlideShow.data).to.equal('function');
+  });
+
+  it('currentIndex === 0', () => {
+    expect(defaultData.currentIndex).to.equal(0);
+  });
+
+  it('paginations.length === 0', () => {
+    expect(defaultData.paginations.length).to.equal(0);
+  });
+
+  it('timer === null', () => {
+    expect(defaultData.timer).to.equal(null);
+  });
+});
